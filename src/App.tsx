00ed1f2ce8494f179317e9fd4bd681df01bd10e6
Bruno Lima <brunolimadevelopment@gmail.com>
2022-06-23 02:48:44 -0300

    component lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
+import { Event } from "./pages/Event"
 
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-// tipando o objeto lesson
-interface Lesson {
-  id: string;
-  title: string;
-}
-
-// dentro do data tem o objeto lesson em seguida o array de lesson
-// <{ lessons: Lesson[] }>
 function App() {
 
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  // caso a entidade lesson da query não exista o data vai retornar undefined ai vai dar erro, então é preciso adicionar o ?
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
    
 }
